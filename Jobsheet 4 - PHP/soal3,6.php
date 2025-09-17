f12cf5cae0501011ef1e691d43bb6a5df89d9a41
PraNAtaP <pranatapu08@gmail.com>
2025-09-17 20:36:52 +0700

    Soal Cerita 3.6 - Kursi Restoran

--- Jobsheet 4 - PHP/soal3,6.php
+++ Jobsheet 4 - PHP/soal3,6.php
@@ -2,9 +2,9 @@
 $kuota = 45;
 $ditempati = 28;
 $kursiKosong = $kuota - $ditempati;
-$persentase = ($ditempati / $kuota) * 100;
+$persentase = ($kursiKosong / $kuota) * 100;
 
 echo "Jumlah kursi: {$kuota} <br>";
 echo "Jumlah kursi yang ditempati: {$ditempati} <br>";
 echo "Jumlah kursi yang kosong: {$kursiKosong} <br>";
-echo "Persentase kursi yang ditempati: {$persentase}% <br>";
+echo "Persentase kursi yang masih kosong: {$persentase}% <br>";
