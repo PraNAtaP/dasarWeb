757f770c83b96f7e2972cd5927537e4bdedb5743
PraNAtaP <pranatapu08@gmail.com>
2025-09-25 20:28:21 +0700

    Praktikum 2 - Associative Array (Styling)

--- Jobsheet 5 - PHP 2/array_2.php
+++ Jobsheet 5 - PHP 2/array_2.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    sty
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #fc009cff;
+        }
+    </style>
 </head>
+
 <body>
     <?php
     $Dosen = [
@@ -13,10 +26,27 @@
         'domisili' => 'Malang',
         'jenis_kelamin' => 'Perempuan'
     ];
-
-    echo "Nama : {$Dosen['nama']} <br>";
-    echo "Domisili : {$Dosen['domisili']} <br>";
-    echo "Jenis Kelamin : {$Dosen['jenis_kelamin']} <br>";
     ?>
+
+    <h2>DATA DOSEN</h2>
+    <table>
+        <tr>
+            <th>Informasi</th>
+            <th>Detail</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
+
 </html>
