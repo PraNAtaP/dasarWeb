a6e29c2f4d5d175d9c7535d1c4a0e883a2ce3fce
PraNAtaP <pranatapu08@gmail.com>
2025-09-25 20:09:20 +0700

    Praktikum 1 - Indexed Arrays (Loop)

--- Jobsheet 5 - PHP 2/array_1.php
+++ Jobsheet 5 - PHP 2/array_1.php
@@ -6,11 +6,11 @@
 <body>
     <h2>Array Terindeks</h2>
     <?php
-    $Listdosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagas Nugraha"];
-
-    echo $Listdosen[2] . "<br>";
-    echo $Listdosen[0] . "<br>";
-    echo $Listdosen[1] . "<br>";
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
     ?>
 </body>
 </html>
