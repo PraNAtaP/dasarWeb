85aab01e3c92971e17ed1aace6ebfa4a2a26ed2e
PraNAtaP <pranatapu08@gmail.com>
2025-09-28 13:02:35 +0700

    Praktikum 7 - String (part 1)

---
+++ Jobsheet 5 - PHP 2/string1.php
@@ -0,0 +1,12 @@
+<?php
+$loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Pellentesque et mi orci. In hac habitasse platea dictumst.";
+
+echo "<p>{$loremIpsum}</p>";
+echo "Panjang Karakter : ". strlen($loremIpsum) ."<br>";
+echo "Panjang Kata : ". str_word_count($loremIpsum) ."<br>";
+echo "<p>" . strtoupper($loremIpsum) . "</p>";
+echo "<p>" . strtolower($loremIpsum) . "</p>";
+
+?>
+
