5e3cd85a2b7ec4c148a2a9aeb3d0abd821ae442c
PraNAtaP <pranatapu08@gmail.com>
2025-10-06 10:37:21 +0700

    Praktikum 5 - Regex (part 4)

--- Jobsheet 7 - PHP Form/regex.php
+++ Jobsheet 7 - PHP Form/regex.php
@@ -1,7 +1,9 @@
 <?php
-$pattern = '/apple/';
-$replacement = 'banana';
-$text = 'I like apple pie';
-$newText = preg_replace($pattern, $replacement, $text);
-echo $newText;
+$pattern = '/go*d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan : $matches[0]";
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
