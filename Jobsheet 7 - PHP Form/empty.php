468e19345cfed0cb08978c3cee4c028dce536647
PraNAtaP <pranatapu08@gmail.com>
2025-10-06 09:04:03 +0700

    Praktikum 2 - Function Empty

---
+++ Jobsheet 7 - PHP Form/empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array();
+if (empty($myArray)) {
+    echo "Array tidak terdefinisikan atau kosong.";
+} else {
+    echo "Array terdefinisikan dan tidak kosong.";
+}
+
+echo "<br/>";
+if (empty($nonExixtentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong";
+}
