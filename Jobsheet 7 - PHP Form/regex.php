e9bc6ac1370285aad9ee681a29ca2ad271da058d
PraNAtaP <pranatapu08@gmail.com>
2025-10-06 10:31:55 +0700

    Praktikum 5 - Regex (part 3)

--- Jobsheet 7 - PHP Form/regex.php
+++ Jobsheet 7 - PHP Form/regex.php
@@ -1,8 +1,7 @@
 <?php
-$pattern = '/[0-9]/';
-$text = 'There are 123 apples';
-if (preg_match($pattern, $text, $matches)) {
-    echo "Cocokkan : " . $matches[0];
-} else {
-    echo "Tidak ada yang cocok";
-}
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie';
+$newText = preg_replace($pattern, $replacement, $text);
+echo $newText;
+?>
